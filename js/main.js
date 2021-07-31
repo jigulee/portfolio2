f68ffbd9e767c4ceadba4caecb570fd83c95c741
jigulee <jigulee@naver.com>
2021-07-31 17:38:56 +0900

    슬라이더 디자인 수정

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const swiper = new Swiper("#wrap", { //2번째 인수값을 객체로 전달
     loop: true, //순환
     slidesPerView: "auto", //지정한 넓이값 적용
     centeredSlides: true, //활성화된 값 가운데로 배치
-    spaceBetween: 50, //사이 간격 벌리기
+    spaceBetween: 60, //사이 간격 벌리기
     mousewheel: true, //페이지 마우스휠로 넘기기
 
     navigation: { //페이지 넘기는 버튼
